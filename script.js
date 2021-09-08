21f81f8a5f991dc5e786aa747f5e0534ff5c0260
Lynn Göttgens <57255030+lynnxg@users.noreply.github.com>
2021-09-08 09:58:34 +0000

    stuiterend

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-var x, y, v;
+var x, y, xv, yv;
 
 function setup() {
 	createCanvas(500, 400);
   x = 0;
-  y = 200;
-  v = 100;
+  y = 10;
+  xv = 10;
+  yv = 10;
 }
 
 function draw() {
   background(225);
 
   ellipse(x,y,50,50);
-  x = x + v;
+  x = x + xv;
+  y = y + yv;
 
   if(x <= 0 || x > 500){
-    v = v * -1;
+    xv = xv * -1;
   }
+
+  if(y <= 0 || y > 400){
+    yv = yv * -1;
+  }
+
+
 }
